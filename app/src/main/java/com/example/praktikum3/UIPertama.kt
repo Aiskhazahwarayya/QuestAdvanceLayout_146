b51c452cf4d341f3819d7496a922ec6406656989
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-23 16:40:39 +0700

    berfungsi sebagai container utama.

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -41,6 +41,8 @@ fun ActivitasPertama(modifier: Modifier){
         Text( stringResource( R.string.univ),
             fontSize = 22.sp)
         Spacer(modifier = Modifier.height( 25.dp))
+
+        //card
         Card (modifier = Modifier
             .fillMaxWidth( 1f)
             .padding( all = 12.dp),

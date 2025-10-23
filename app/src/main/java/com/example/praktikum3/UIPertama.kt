e65c0070bd71d8ea0ef7338fda9eed013e091dbc
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-23 16:30:33 +0700

    menambahkan gambar atau logo di sisi kiri kartu.

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -47,7 +47,17 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(containerColor = Color.DarkGray
             )
         ){
-            //
+            Row() {
+                val gambar = painterResource(R.drawable.logoumy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(5.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+            }
         }
     }
 }

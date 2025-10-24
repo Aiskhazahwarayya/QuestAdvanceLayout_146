d85d669b11938469119d126dd72eae6de40258ba
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-24 07:07:50 +0700

    Menambahkan Spacer untuk memberi jarak antara gambar dan kolom teks

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -99,6 +99,8 @@ fun ActivitasPertama(modifier: Modifier){
                         .size(100.dp)
                         .padding(5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
+
 
             }
 
